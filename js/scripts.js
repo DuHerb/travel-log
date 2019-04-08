c7751795347bb9c2a9ed69f3ffe3d847f7962e2c
Dustin Herboldshimer and Liz Kelley <dustin@>
2019-04-08 10:26:11 -0700

    add seattle object, add city buttons, display values

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
-function Place (name){
-  this.name = name,
+function Place() {
+  name = name,
   country = "",
   population = 0,
   landmark = "",
@@ -8,17 +8,37 @@ function Place (name){
 
 var visitedLocations = [];
 
-var portland = new Place ("Portland");
+var portland = new Place();
+portland.name = "Portland";
 portland.country = "USA";
 portland.population = 600000;
 portland.landmark = "Japanese Garden";
 portland.notes = "Rains Alot";
 visitedLocations.push(portland);
 
+var seattle = new Place();
+seattle.name = "Seattle";
+seattle.country = "USA";
+seattle.population = 1000000;
+seattle.landmark = "space needle";
+seattle.notes = "Rains Alot";
+visitedLocations.push(seattle);
 
+function displayPlace(place) {
+  $("#place-display").append("<li>" + place.name + "</li>");
+  $("#place-display").append("<li>" + place.country + "</li>");
+  $("#place-display").append("<li>" + place.population + "</li>");
+  $("#place-display").append("<li>" + place.landmark + "</li>");
+  $("#place-display").append("<li>" + place.notes + "</li>");
+}
 
-$(document).ready(function(){
-  console.log(portland);
-  console.log(portland.country);
-  console.log(visitedLocations[0]);
-})
+function displayCities(visitedLocations) {
+  for(i=0; i<visitedLocations.length; i++) {
+    $("#place-list").append('<li><button class="city-button">' + visitedLocations[i].name + '</button></li>');
+  };
+}
+
+$(document).ready(function() {
+  console.log(visitedLocations);
+  displayCities(visitedLocations);
+});
